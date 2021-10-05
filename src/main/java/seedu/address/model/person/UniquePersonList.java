ceeadff13b50a72963a8ad8f66e456ee7b1f20d7
Mohamed-Noriman <seaweed.iman@gmail.com>
2021-10-05 16:51:01 +0800

    Fix checkstyle. pls work omg

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -134,12 +134,4 @@ public class UniquePersonList implements Iterable<Person> {
         }
         return true;
     }
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for(Person p : internalList) {
-            sb.append(p.toString() + "\n");
-        }
-        return sb.toString();
-    }
 }
