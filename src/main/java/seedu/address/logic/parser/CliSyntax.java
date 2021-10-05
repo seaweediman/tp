25c222826276414b6cebfea5abbb9a42532cc977
Mohamed-Noriman <seaweed.iman@gmail.com>
2021-10-05 14:43:11 +0800

    Fix RemarkCommandTest

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -4,7 +4,6 @@ package seedu.address.logic.parser;
  * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
  */
 public class CliSyntax {
-
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("name=");
     public static final Prefix PREFIX_PHONE = new Prefix("phone=");
@@ -12,5 +11,4 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("address=");
     public static final Prefix PREFIX_TAG = new Prefix("tag=");
     public static final Prefix PREFIX_REMARK = new Prefix("remark=");
-
 }
