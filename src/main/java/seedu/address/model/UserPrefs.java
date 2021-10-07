6087f8202407a2113cb6fdd528025d5cd56f2d97
sueann-chua <sue.ann.chua@gmail.com>
2021-10-08 01:32:43 +0800

    Change filepath for storage

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -14,7 +14,7 @@ import seedu.address.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path hrManagerCandidatesFilePath = Paths.get("data" , "addressbook.json"); //TODO
+    private Path hrManagerCandidatesFilePath = Paths.get("data" , "candidates.json"); //TODO
 
     /**
      * Creates a {@code UserPrefs} with default values.
