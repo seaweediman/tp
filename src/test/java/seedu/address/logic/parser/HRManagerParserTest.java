4c70e1596d70a4ba91660783c3d4a20e707cbd8c
sueann-chua <sue.ann.chua@gmail.com>
2021-10-07 22:53:08 +0800

    Change references from AddressBookParser to HRManagerParser

--- src/test/java/seedu/address/logic/parser/HRManagerParserTest.java
+++ src/test/java/seedu/address/logic/parser/HRManagerParserTest.java
@@ -29,9 +29,9 @@ import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.PersonUtil;
 
-public class AddressBookParserTest {
+public class HRManagerParserTest {
 
-    private final AddressBookParser parser = new AddressBookParser();
+    private final HRManagerParser parser = new HRManagerParser();
 
     @Test
     public void parseCommand_add() throws Exception {
