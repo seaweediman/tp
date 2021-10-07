e90e592c73b42f1d5924ac259ad390aa32c28148
angnobel <nobelang78@gmail.com>
2021-10-07 23:52:00 +0800

    Update message to remove enum reference

--- src/main/java/seedu/address/model/person/Status.java
+++ src/main/java/seedu/address/model/person/Status.java
@@ -9,7 +9,7 @@ public enum Status {
     REJECTED (5, true),
     WITHDRAWN (6, true);
 
-    public static final String MESSAGE_CONSTRAINTS = "Enums can take the value:\n0.NONE\n1."
+    public static final String MESSAGE_CONSTRAINTS = "Status can take the values:\n0.NONE\n1."
             + "APPLIED\n2.SCHEDULED"
             + "\n3.INTERVIEWED\n4.ACCEPTED\n5.REJECTED\n6.WITHDRAWN";
 
