3cc078bbd5f66ca6534217de1d130c0c6f87cf68
JeffZincatz <jeffzincatz@gmail.com>
2021-10-09 22:35:27 +0800

    Add UniquePositionList and test cases

---
+++ src/main/java/seedu/address/model/position/UniquePositionList.java
@@ -0,0 +1,138 @@
+package seedu.address.model.position;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.model.position.exceptions.DuplicatePositionException;
+import seedu.address.model.position.exceptions.PositionNotFoundException;
+
+/**
+ * A list of positions that enforces uniqueness between its elements and does not allow nulls.
+ * A position is considered unique by comparing using {@code Position#isSamePosition(Position)}. As such, adding and
+ * updating of positions uses Position#isSamePosition(Position) for equality so as to ensure that the position being
+ * added or updated is unique in terms of identity in the UniquePositionList. However, the removal of a position uses
+ * Position#equals(Object) so as to ensure that the position with exactly the same fields will be removed.
+ * <p>
+ * Supports a minimal set of list operations.
+ *
+ * @see Position#isSamePosition(Position)
+ */
+public class UniquePositionList implements Iterable<Position> {
+
+    private final ObservableList<Position> internalList = FXCollections.observableArrayList();
+    private final ObservableList<Position> internalUnmodifiableList =
+            FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Returns true if the list contains an equivalent position as the given argument.
+     */
+    public boolean contains(Position toCheck) {
+        requireNonNull(toCheck);
+        return internalList.stream().anyMatch(toCheck::isSamePosition);
+    }
+
+    /**
+     * Adds a position to the list.
+     * The position must not already exist in the list.
+     */
+    public void add(Position toAdd) {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicatePositionException();
+        }
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Replaces the position {@code target} in the list with {@code editedPosition}.
+     * {@code target} must exist in the list.
+     * The position identity of {@code editedPosition} must not be the same as another existing position in the list.
+     */
+    public void setPosition(Position target, Position editedPosition) {
+        requireAllNonNull(target, editedPosition);
+
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            throw new PositionNotFoundException();
+        }
+
+        if (!target.isSamePosition(editedPosition) && contains(editedPosition)) {
+            throw new DuplicatePositionException();
+        }
+
+        internalList.set(index, editedPosition);
+    }
+
+    /**
+     * Removes the equivalent position from the list.
+     * The position must exist in the list.
+     */
+    public void remove(Position toRemove) {
+        requireNonNull(toRemove);
+        if (!internalList.remove(toRemove)) {
+            throw new PositionNotFoundException();
+        }
+    }
+
+    public void setPositions(seedu.address.model.position.UniquePositionList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    /**
+     * Replaces the contents of this list with {@code positions}.
+     * {@code positions} must not contain duplicate positions.
+     */
+    public void setPositions(List<Position> positions) {
+        requireAllNonNull(positions);
+        if (!positionsAreUnique(positions)) {
+            throw new DuplicatePositionException();
+        }
+
+        internalList.setAll(positions);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<Position> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    @Override
+    public Iterator<Position> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof seedu.address.model.position.UniquePositionList // instanceof handles nulls
+                && internalList.equals(((seedu.address.model.position.UniquePositionList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+    /**
+     * Returns true if {@code positions} contains only unique positions.
+     */
+    private boolean positionsAreUnique(List<Position> positions) {
+        for (int i = 0; i < positions.size() - 1; i++) {
+            for (int j = i + 1; j < positions.size(); j++) {
+                if (positions.get(i).isSamePosition(positions.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
