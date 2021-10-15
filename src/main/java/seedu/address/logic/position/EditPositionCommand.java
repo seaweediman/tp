173cd7e85206a9eede5fcf654d1ad22328eb9858
sueann-chua <sue.ann.chua@gmail.com>
2021-10-15 23:09:25 +0800

    Rearrange import by lexicographical order

--- src/main/java/seedu/address/logic/position/EditPositionCommand.java
+++ src/main/java/seedu/address/logic/position/EditPositionCommand.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.position;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POSITION_STATUS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_POSITIONS;
 
